6363cf21e014067374b0ac9f2fc9245a124c0aab
Itloopers <micwtn@gmail.com>
2018-03-15 00:29:08 +0100

    StringBuilder objects in loops.     enum type State czanged to "x" and " " without additional space, its added in method witch returned representation.

--- src/gameOfLife/MyGame.java
+++ src/gameOfLife/MyGame.java
@@ -27,7 +27,7 @@ class Board{
     }
 
     void nextGen() {
-        //toString()  have empty field initialization
+        //toString() have empty field initialization
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 board[i][j].getPair(board);
